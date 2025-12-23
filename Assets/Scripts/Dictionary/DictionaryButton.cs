5ae6277e27b8056b41f340a82b92d8c839e9faa3
DrFeelGoodz <agabryluk3@gatech.edu>
2025-12-23 15:00:05 -0500

    Added dictionary prefab and menu to spelling bee

--- Assets/Scripts/Dictionary/DictionaryButton.cs
+++ Assets/Scripts/Dictionary/DictionaryButton.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class DictionaryButton : MonoBehaviour
 {
     public TMP_Text sign;
+    public DictionaryController controller;
     
     public void SetSign(string s)
     {
@@ -11,6 +12,6 @@ public class DictionaryButton : MonoBehaviour
     }
     public void Click()
     {
-        Debug.Log("Clicked: " + sign.text);
+        controller.LoadVideoPage(sign.text);
     }
 }
--- Assets/Scripts/Dictionary/DictionaryController.cs
+++ Assets/Scripts/Dictionary/DictionaryController.cs
@@ -4,6 +4,7 @@ using System.IO;
 using TMPro;
 using UnityEngine.UI;
 using System;
+using UnityEngine.Video;
 
 public class DictionaryController : MonoBehaviour
 {
@@ -17,11 +18,21 @@ public class DictionaryController : MonoBehaviour
     public GameObject buttonPrefab;
     public TMP_Text title;
     public Image header;
+    public TMP_InputField textInput;
+    public GameObject videoPage;
+    public VideoPlayer videoPlayer;
+    public TMP_Text videoTitle;
+    public Image sliderFill;
+    public Image turtle;
+    public Image rabbit;
 
     void Start()
     {
         title.text = dictionaryTitle;
         header.color = headerColor;
+        sliderFill.color = headerColor;
+        rabbit.color = headerColor;
+        turtle.color = headerColor;
         string path = Path.Combine(Application.streamingAssetsPath, signListFile);
         List<string> fullList = new List<string>(File.ReadAllLines(path));
 
@@ -29,6 +40,38 @@ public class DictionaryController : MonoBehaviour
         {
             GameObject button = Instantiate(buttonPrefab, container.transform);
             button.GetComponent<DictionaryButton>().SetSign(sign);
+            button.GetComponent<DictionaryButton>().controller = this;
         }
+    }
+
+    public void FilterSigns()
+    {
+        foreach (Transform child in container.transform)
+        {
+            child.gameObject.SetActive(child.GetComponent<DictionaryButton>().sign.text.ToLower().Contains(textInput.text.ToLower()));
+        }
+    }
+
+    public void LoadVideoPage(string sign)
+    {
+        videoPage.SetActive(true);
+        videoTitle.gameObject.SetActive(true);
+        videoTitle.text = sign;
+        textInput.gameObject.SetActive(false);
+        videoPlayer.url = Application.streamingAssetsPath + $"/MacarthurBates/{sign}.mp4";
+        videoPlayer.Play();
+    }
+
+    public void ExitVideoPage()
+    {
+        videoPlayer.Stop();
+        videoPage.SetActive(false);
+        videoTitle.gameObject.SetActive(false);
+        textInput.gameObject.SetActive(true);
+    }
+
+    public void SpeedControl(Slider slider)
+    {
+        videoPlayer.playbackSpeed = slider.value;
     }
 }
